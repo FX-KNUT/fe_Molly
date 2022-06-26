2b78885a9b68deedea116f31db7fcbcd71d6c3db
KangMin Her <81793748+nicecarrot@users.noreply.github.com>
2022-06-26 22:31:22 +0900

    8 preload 발생

--- frontend/electron/main/index.ts
+++ frontend/electron/main/index.ts
@@ -1,77 +1,79 @@
-import { app, BrowserWindow, shell } from 'electron'
-import { release } from 'os'
-import { join } from 'path'
+import { app, BrowserWindow, shell } from "electron";
+import { release } from "os";
+import { join } from "path";
 
 // Disable GPU Acceleration for Windows 7
-if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+if (release().startsWith("6.1")) app.disableHardwareAcceleration();
 
 // Set application name for Windows 10+ notifications
-if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+if (process.platform === "win32") app.setAppUserModelId(app.getName());
 
 if (!app.requestSingleInstanceLock()) {
-  app.quit()
-  process.exit(0)
+  app.quit();
+  process.exit(0);
 }
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 
-let win: BrowserWindow | null = null
+let win: BrowserWindow | null = null;
 // Here you can add more preload scripts
-const splash = join(__dirname, '../preload/splash.js')
+const splash = join(__dirname, "../preload/splash.js");
 // 🚧 Use ['ENV_NAME'] to avoid vite:define plugin
-const url = `http://${process.env['VITE_DEV_SERVER_HOST']}:${process.env['VITE_DEV_SERVER_PORT']}`
+// const url = `http://${process.env['VITE_DEV_SERVER_HOST']}:${process.env['VITE_DEV_SERVER_PORT']}`
+// 개발을 할 때에는 localhost 3000을 쓰고 이후에는 다시 바꾸자
+const url = `http://127.0.0.1:7777/`;
 
 async function createWindow() {
   win = new BrowserWindow({
-    title: 'Main window',
+    title: "Main window",
     webPreferences: {
       preload: splash,
       nodeIntegration: true,
       contextIsolation: false,
     },
-  })
+  });
 
   if (app.isPackaged) {
-    win.loadFile(join(__dirname, '../../index.html'))
+    win.loadFile(join(__dirname, "../../index.html"));
   } else {
-    win.loadURL(url)
+    win.loadURL(url);
     // win.webContents.openDevTools()
   }
 
   // Test active push message to Renderer-process
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString());
+  });
 
   // Make all links open with the browser, not with the application
   win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url)
-    return { action: 'deny' }
-  })
+    if (url.startsWith("https:")) shell.openExternal(url);
+    return { action: "deny" };
+  });
 
   // 상단 메뉴바 지우기
   win.setMenu(null);
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  win = null
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('second-instance', () => {
+app.on("second-instance", () => {
   if (win) {
     // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore()
-    win.focus()
+    if (win.isMinimized()) win.restore();
+    win.focus();
   }
-})
+});
 
-app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows()
+app.on("activate", () => {
+  const allWindows = BrowserWindow.getAllWindows();
   if (allWindows.length) {
-    allWindows[0].focus()
+    allWindows[0].focus();
   } else {
-    createWindow()
+    createWindow();
   }
-})
+});
